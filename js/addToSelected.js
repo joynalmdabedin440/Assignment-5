e5480e16bdf86cdc8bbeebceebdab7d22e656b8b
joynalmdabedin <joynalmdabedin440@gmail.com>
2022-08-23 11:18:31 +0600

    selected player add done

---
+++ js/addToSelected.js
@@ -0,0 +1,33 @@
+
+const selectedPlayer = [];
+const selectedPlayerLength = selectedPlayer.length;
+
+
+function showAsSelected(playerName) {
+    const selectedPlayerTable = document.getElementById('selected-player-table');
+    selectedPlayerTable.innerHTML = '';
+
+    for (let i = 0; i < selectedPlayer.length; i++){
+        const name = selectedPlayer[i]
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+        <th>${i + 1}</th>
+        <td>${name}`;
+        selectedPlayerTable.appendChild(tr)
+    }
+}
+ 
+function addToSelected(selectedName) {
+    const playerName = selectedName.parentNode.parentNode.children[0].innerText;
+    
+    selectedPlayer.push(playerName);
+
+    const selectedIndex = document.getElementById('selected-index');
+    const selectedPlayerLength = selectedPlayer.length;
+    if (selectedPlayerLength>5) {
+       alert('You Can\'t add more Player!!!')
+       return selectedPlayerLength;
+   }
+    selectedIndex.innerText = selectedPlayerLength;
+    showAsSelected(playerName);
+}
---
+++ js/utilities.js
@@ -0,0 +1,3 @@
+document.getElementById('player-calculate-btn').addEventListener('click', function () {
+    const perPlayerInput=document.getElementById('player-input-field')
+})
